0f2174b8444cdfb8ba0b896de786777a82b0488b
Afzaal Khan <162747532+afzaalkhan27@users.noreply.github.com>
2024-03-26 02:27:58 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,101 @@
+//string
+
+let userName:  string =  "Afzaalkhan";
+console.log(userName)
+let favouriteColour: string = "Black";
+console.log(favouriteColour)
+let favouriteFood: string = "Desifood";
+console.log(favouriteFood)
+let favouriteDish: string = "chicken tikka biryani";
+console.log(favouriteDish)
+let favouriteSweet: string = "Gulabjamun";
+console.log(favouriteSweet)
+let favouritecolddrink: string =  "pepsi";
+console.log(favouritecolddrink)
+let favouriteCar: string = "vigodala";
+console.log(favouriteCar)
+let favouriteIslamicmonth: string = "Ramadan Kareem";
+console.log(favouriteIslamicmonth)
+let favouriteLeader: string = "Qaid-e-Azam";
+console.log(favouriteLeader)
+let favouriteClothingbrand: string = "J.";
+console.log(favouriteClothingbrand)
+let favouriteCartroon: string = "Tom&jarry";
+console.log(favouriteCartroon)
+let myBirthday: string = "27-December";
+console.log(myBirthday)
+let myfavouritePersoninthisWorld: string = "my sweet mom";
+console.log(myfavouritePersoninthisWorld)
+let myDreamCountry: string = "Makkah City";
+console.log(myDreamCountry)
+let myfavouriteCity: string = "Digri (hometown";
+console.log(myfavouriteCity)
+let myfavouriteAnimal: string ="Cat";
+console.log(myfavouriteAnimal)
+let favouriteBird: string ="perot";
+console.log(favouriteBird)
+let favouriteFriut: string ="mango";
+console.log(favouriteFriut)
+let favouriteShake: string = "mango shake";
+console.log(favouriteShake)
+let favouriteIslamicCaller: string = "'Molana ilyas Qadri', peer Ajmal sahab ";
+console.log(favouriteIslamicCaller)
+let favouriteAirport: string = "jeddha airport";
+console.log(favouriteAirport)
+let favouriteBook: string = "How to Like Yourself";
+console.log(favouriteBook)
+let favouriteCricketPlayer: string = "boom boom Sahid khan afridi";
+console.log(favouriteCricketPlayer)
+
+//Number
+
+let yourBirthdayDate: number = 27/12/1999;
+console.log(yourBirthdayDate)
+let currentYear: number = 2024;
+console.log(currentYear)
+let currentUsdTOPkr: Number = 278.33;
+console.log(currentUsdTOPkr)
+let yourPhoneNumber: Number = 92+3353642550;
+console.log(yourPhoneNumber)
+let age: number = 22;
+console.log(age)
+let hight: number = 5.4;
+console.log(hight)
+
+//Boolean
+
+let isMarried: Boolean = false;
+console.log(isMarried)
+let ahmed_MyBestFriend: boolean = true;
+console.log(ahmed_MyBestFriend)
+let have_aPet: boolean = true;
+console.log(have_aPet)
+let is_Nawazsharif_chor: boolean = true;
+console.log(is_Nawazsharif_chor)
+let Sharks_are_mammals: boolean = false;
+console.log(Sharks_are_mammals)
+let SouthAfrica_has_one_capital: boolean = false;
+console.log(SouthAfrica_has_one_capital)
+let Number_is_textual_data: boolean = false;
+console.log(Number_is_textual_data)
+let string_is_textual_data: boolean = true;
+console.log(string_is_textual_data)
+let my_MOM_my_world: Boolean = true;
+console.log(my_MOM_my_world)
+let The_human_body_has_four_legs: Boolean = false;
+console.log(The_human_body_has_four_legs)
+let The_human_eye_can_distinguish_10_million_different_colors: Boolean = true;
+console.log(The_human_eye_can_distinguish_10_million_different_colors)
+let Pepperoni_is_the_most_popular_pizza_topping_in_the_Us: Boolean = true;
+console.log(Pepperoni_is_the_most_popular_pizza_topping_in_the_Us)
+
+//any
+
+let randomValue: any =  "https://www.example.com/behavior/authority.html";
+console.log(randomValue)
+
+let country2: null = null;
+console.log(country2);
+
+ const accountNumber: number= 123456789;
+console.log(accountNumber)
